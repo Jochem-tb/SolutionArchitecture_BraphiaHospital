a014bb2d391e9712c97b1b2045e221cd05308a73
Zaid Karmoudi <150630253+Zaidkar@users.noreply.github.com>
2026-06-18 11:40:04 +0200

    Workshop template

---
+++ src/IdentityApi/Program.cs
@@ -0,0 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.EntityFrameworkCore;
+using OnlineToestemming.IdentityApi;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+
+builder.Services.AddOpenApi();
+
+builder.Configuration.AddEnvironmentVariables();
+
+builder
+    .Services.AddTransient<IAuthenticationService, AuthenticationService>()
+    .Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineToestemmingDb"))
+);
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapAuthEndpoints();
+
+app.Run();
